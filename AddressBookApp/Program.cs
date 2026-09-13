a25acd63f29bb74bd8c2f357e95a235a2d51b9e5
harshdeep1841 <harshdeepsingh10d@gmail.com>
2026-09-13 19:54:31 +0530

    UC3: add contacts to address book

---
+++ AddressBookApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using AddressBook.Exceptions;
+using AddressBook.Models;
+
+
+
+AddressBook.Services.AddressBook addressBook = new AddressBook.Services.AddressBook();
+while (true)
+{
+    Console.WriteLine();
+    Console.WriteLine("===== ADDRESS BOOK =====");
+    Console.WriteLine("1. Add Contact");
+    Console.WriteLine("2. Show All Contacts");
+    Console.WriteLine("0. Exit");
+    Console.Write("Enter your choice: ");
+
+    string? choice = Console.ReadLine();
+
+    if (choice == "0")
+    {
+        break;
+    }
+
+    if (choice == "1")
+    {
+        try
+        {
+            Console.Write("Enter first name: ");
+            string firstName = Console.ReadLine()!;
+
+            Console.Write("Enter last name: ");
+            string lastName = Console.ReadLine()!;
+
+            Console.Write("Enter address: ");
+            string address = Console.ReadLine()!;
+
+            Console.Write("Enter city: ");
+            string city = Console.ReadLine()!;
+
+            Console.Write("Enter state: ");
+            string state = Console.ReadLine()!;
+
+            Console.Write("Enter zip: ");
+            string zip = Console.ReadLine()!;
+
+            Console.Write("Enter phone number: ");
+            string phoneNumber = Console.ReadLine()!;
+
+            Console.Write("Enter email: ");
+            string email = Console.ReadLine()!;
+
+            Contact contact = new Contact(
+                firstName,
+                lastName,
+                address,
+                city,
+                state,
+                zip,
+                phoneNumber,
+                email
+            );
+
+            addressBook.AddContact(contact);
+
+            Console.WriteLine("Contact added successfully.");
+        }
+        catch (InvalidContactException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+    else if (choice == "2")
+    {
+        addressBook.PrintAll();
+    }
+    else
+    {
+        Console.WriteLine("Invalid choice.");
+    }
+}
